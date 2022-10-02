a921e7de3775a033094f0fc96019a537852e0f43
lakshmankumar <laxmansgvp@gmail.com>
2022-10-02 19:03:49 +0530

    adding file to master

---
+++ day14/Day14LinkedList/App.java
@@ -0,0 +1,13 @@
+package day14.Day14LinkedList;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
